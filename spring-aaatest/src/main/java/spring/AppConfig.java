cc0b69c3abbec3a7b03bf020a8b5ab6955d7c8b0
kongjiang <470922596@qq.com>
2020-01-04 16:23:59 +0800

    更新

--- spring-aaatest/src/main/java/spring/AppConfig.java
+++ spring-aaatest/src/main/java/spring/AppConfig.java
@@ -6,6 +6,12 @@ import org.springframework.context.annotation.Configuration;
 
 @ComponentScan("spring")
 @Configuration
+/**
+ * lite @Bean mode ：当@Bean方法在没有使用@Configuration注解的类中声明时称之为lite @Bean mode
+ * Full @Configuration：如果@Bean方法在使用@Configuration注解的类中声明时称之为Full @Configuration   这样的会做cglib动态代理
+ * 主要根据是否有@Configuration来决定（spring的full和lite：full就是全配置类 使用@Configuration  lite就是部分配置类 不用该注解） 存在则cglib动态代理  不存在就跳过
+ * 源码在ConfigurationClassPostProcessor类postProcessBeanFactory()方法中
+ */
 public class AppConfig {
 
 	static{
